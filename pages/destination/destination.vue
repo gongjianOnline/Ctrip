8e8614491ead9b468625ff7a4e8ac9a4e3bacd8c
李承斌 <15031617367@163.com>
2020-10-01 21:06:42 +0800

    导航栏设置

--- pages/destination/destination.vue
+++ pages/destination/destination.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="content">
-		首页
+	<view>
+		目的地页面
 	</view>
 </template>
 
@@ -8,18 +8,15 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			
 		}
 	}
 </script>
 
 <style>
-	
+
 </style>
